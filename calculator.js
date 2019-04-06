346064e317106d65953891c6b5948d32b959e84e
programacctest <programacctest@protonmail.com>
2019-04-06 14:41:36 +0200

    Made Program More Modular
    Put all that stuff into some functions.

--- calculator.js
+++ calculator.js
@@ -1,71 +1,60 @@
-calculator = function (tokenArray) {
+array = [2, '*', 3, '^', 2, '+', 4, '/', 3];
+result = calculator (array);
+document.querySelector('#result').innerHTML = result;
+
+function calculator (tokenArray) {
   console.log ('Initial:');
   console.log (tokenArray);
 
   //Calculate first level: exponents (^)
-  newArray = [];
-  for (index = 0; index < tokenArray.length; index++) {
-    token = tokenArray[index]
-    if ((tokenArray[index+1]) == '^') {
-      index += 2
-      nextToken = tokenArray[index];
-      newArray.push (Math.pow (token, nextToken));
-    }
-    else {
-      newArray.push (token);
-    }
-  }
-  tokenArray = newArray;
-  console.log ('After ^ processing:');
+  tokenArray = calclevel (tokenArray, ['^']);
+  console.log ('After ' + ['^'] + ' processing:');
   console.log (tokenArray);
 
   //Calculate second level: multiplication & division (*, /)
-  newArray = [];
-  for (index = 0; index < tokenArray.length; index++) {
-    token = tokenArray[index]
-    if ((action = tokenArray[index+1]) == '*' || action == '/') {
-      index += 2
-      nextToken = tokenArray[index];
-      if (action == '*') {
-        newArray.push (token * nextToken);
-      }
-      else { //must be '/'
-        newArray.push (token / nextToken);
-      }
-    }
-    else {
-      newArray.push (token);
-    }
-  }
-  tokenArray = newArray;
-  console.log ('After *,/ processing:');
+  tokenArray = calclevel (tokenArray, ['*', '/']);
+  console.log ('After ' + ['*', '/'] + ' processing:');
   console.log (tokenArray);
 
   //Calculate second level: addition & subtraction (+, -)
-  newArray = [];
-  for (index = 0; index < tokenArray.length; index++) {
-    token = tokenArray[index]
-    if ((action = tokenArray[index+1]) == '+' || action == '-') {
-      index += 2
-      nextToken = tokenArray[index];
-      if (action == '+') {
-        newArray.push (token + nextToken);
-      }
-      else { //must be '-'
-        newArray.push (token - nextToken);
-      }
-    }
-    else {
-      newArray.push (token);
-    }
-  }
-  tokenArray = newArray;
-  console.log ('After +,- processing:');
+  tokenArray = calclevel (tokenArray, ['+', '-']);
+  console.log ('After ' + ['+', '-'] + ' processing:');
   console.log (tokenArray);
 
   return tokenArray[0];
-};
+}
 
-array = [2, '*', 3, '^', 2, '+', 4, '/', 3];
-result = calculator (array);
-document.querySelector('#result').innerHTML = result;
+function calclevel (oldArray, check) {
+  // check should be an array of tokens to check against
+  // like ['-', '+'] -> must be valid actions in calculateNums ()
+  newArray = [];
+  for (index = 0; index < oldArray.length; index++) {
+    currentToken = oldArray[index];
+    actionToken = oldArray[index+1];
+    if (check.includes (actionToken)) {
+      index += 2
+      nextToken = oldArray[index];
+      calculation = calculateNums (currentToken, nextToken, actionToken);
+      newArray.push (calculation);
+    }
+    else {
+      newArray.push (currentToken);
+    }
+  }
+  return newArray;
+}
+
+function calculateNums (num1, num2, action) {
+  switch (action) {
+    case '^':
+      return Math.pow(currentToken, nextToken);
+    case '*':
+      return currentToken * nextToken;
+    case '/':
+      return currentToken / nextToken;
+    case '+':
+      return currentToken + nextToken;
+    case '-':
+      return currentToken - nextToken;
+  }
+}
